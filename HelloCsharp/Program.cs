702e9d83256a7d853c7b9bd7a1ac2f268307dcdc
P.angelino <p.angelinodaniele@gmail.com>
2022-03-31 16:57:35 +0200

    Primo programma in c#

--- HelloCsharp/Program.cs
+++ HelloCsharp/Program.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Benvenuto nel mio primo programma in c#!");
+Console.WriteLine("GENNAIO");
+Console.WriteLine("FEBBRAIO");
+Console.WriteLine("MARZO");
+Console.WriteLine("APRILE");
+Console.WriteLine("MAGGIO");
+Console.WriteLine("GIUGNO");
+Console.WriteLine("LUGLIO");
+Console.WriteLine("AGOSTO");
+Console.WriteLine("SETTEMBRE");
+Console.WriteLine("OTTOBRE");
+Console.WriteLine("NOVEMBRE");
+Console.WriteLine("DICEMBRE");
+
+Console.WriteLine("Scegli il tuo mese preferito");
+var mesePreferito = Console.ReadLine();
+Console.WriteLine("Il tuo mese preferito è: " +  mesePreferito);
+Console.WriteLine("Grazie per aver scelto il tuo mese preferito, buona giornata.");
+Environment.Exit(0);
+
